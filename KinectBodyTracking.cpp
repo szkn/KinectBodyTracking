a092eb9801cdfb7ec47f80dda63f59b4044a0ad4
Kento <0706kento@gmail.com>
2021-10-12 18:29:12 +0900

    🎉Finish Refactoring & Saving Images
    2D、3Dのデータの出力と画像の保存をしつつ、20以上のFPSを達成

--- KinectBodyTracking.cpp
+++ KinectBodyTracking.cpp
@@ -17,12 +17,14 @@ using namespace std;
 #include <time.h>
 #include <conio.h>
 #include <direct.h>
+#include <Windows.h>
 
 using std::cout; using std::endl;
 using std::chrono::duration_cast;
 using std::chrono::milliseconds;
 using std::chrono::seconds;
 using std::chrono::system_clock;
+
 
 #define FRAME_NUM 10000
 #define VERIFY(result, error)                                                                            \
@@ -31,10 +33,8 @@ using std::chrono::system_clock;
         printf("%s \n - (File: %s, Function: %s, Line: %d)\n", error, __FILE__, __FUNCTION__, __LINE__); \
         exit(1);                                                                                         \
     } 
-
-k4a_calibration_t g_Calibration;			// Azure Kinect のキャリブレーションデータ      
+    
 k4a_float2_t g_fSkeleton2D[K4ABT_JOINT_COUNT] = { 0.0f, };	// ユーザーの 2D 骨格座標 (表示用)
-
 k4a_device_t device = nullptr;
 k4abt_tracker_t tracker = nullptr;  //体のトラッキング結果を取得するためのトラッカーを作成
 k4abt_frame_t body_frame = nullptr;
@@ -42,11 +42,19 @@ k4a_calibration_t sensor_calibration;
 k4abt_tracker_configuration_t tracker_config = K4ABT_TRACKER_CONFIG_DEFAULT;
 k4abt_skeleton_t skeleton;
 
+//関節点を書き込む用のvectorを作成しておく。毎フレーム中に入れるlistを生成する
+vector<list<string>> vec_jointlist_2d; //2D描写データを格納するvector
+vector<list<string>> vec_jointlist; //3Dcamera座標のデータを取得するvector
+vector<cv::Mat_<cv::Vec4b>> vec_image;      //画像データ保存用のvector
+vector<string> vec_time; //画像データが生成された時刻を保存しておくvector
+int frame_count = 0; //現在のFrame位置を記録しておく変数
+uint32_t temp_time = NULL; //FPS算出用の変数
 
 
 
+// 関数群
 // Kinect を初期化する
-k4a_result_t CreateKinect() {    
+k4a_result_t CreateKinect() {
     //Kinectを開く
     VERIFY(k4a_device_open(0, &device), "Open K4A Device failed!");
 
@@ -57,10 +65,22 @@ k4a_result_t CreateKinect() {
     deviceConfig.color_format = K4A_IMAGE_FORMAT_COLOR_BGRA32;
     deviceConfig.camera_fps = K4A_FRAMES_PER_SECOND_30;
     VERIFY(k4a_device_start_cameras(device, &deviceConfig), "Start K4A cameras failed!");
+    //キャリブレーションを取得
     VERIFY(k4a_device_get_calibration(device, deviceConfig.depth_mode, deviceConfig.color_resolution, &sensor_calibration),
         "Get depth camera calibration failed!");
     VERIFY(k4abt_tracker_create(&sensor_calibration, tracker_config, &tracker), "Body tracker initialization failed!");
 
+}
+
+//後片付け
+k4a_result_t DestroyKinect() {
+    k4abt_tracker_shutdown(tracker);
+    k4abt_tracker_destroy(tracker);
+    k4a_device_stop_cameras(device);
+    k4a_device_close(device);
+    cv::destroyAllWindows();
+    printf("\nFinished body tracking processing!\n");
+    return K4A_RESULT_SUCCEEDED;
 }
 
 string CreateSaveDir(std::string timedata) {
@@ -105,7 +125,7 @@ int cap_result(k4a_wait_result_t queue_capture_result) {
     else { return 1; }
 }
 
-list<string> Camera3dto2d(uint32_t during_millisec, uint32_t id){
+list<string> Camera3dto2d(uint32_t during_millisec, uint32_t id) {
     list<string> l_joint_2d;   //キャリブレーションデータを格納するリストの作成
     l_joint_2d.empty();
     l_joint_2d.push_back(std::to_string(during_millisec));
@@ -115,7 +135,7 @@ list<string> Camera3dto2d(uint32_t during_millisec, uint32_t id){
     {
         int iValid = 0;
         // 3D の骨格座標を 2D スクリーン座標に変換する
-        k4a_calibration_3d_to_2d(&g_Calibration, &skeleton.joints[iJoint].position, K4A_CALIBRATION_TYPE_DEPTH, K4A_CALIBRATION_TYPE_COLOR, &g_fSkeleton2D[iJoint], &iValid);
+        k4a_calibration_3d_to_2d(&sensor_calibration, &skeleton.joints[iJoint].position, K4A_CALIBRATION_TYPE_DEPTH, K4A_CALIBRATION_TYPE_COLOR, &g_fSkeleton2D[iJoint], &iValid);
         if (iValid == 0)
         {
             // 無効な値は (0,0) に設定
@@ -129,158 +149,7 @@ list<string> Camera3dto2d(uint32_t during_millisec, uint32_t id){
     return l_joint_2d;
 }
 
-int main()
-{   
-    CreateKinect();
-
-    //計測開始時のtimesramp作成
-    uint32_t start_time = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-    //関節点を書き込む用のvectorを作成しておく。毎フレーム中に入れるlistを生成する
-    vector<list<string>> vec_jointlist_2D; //2D描写データを格納するvector
-    vector<list<string>> vec_jointlist; //3Dcamera座標のデータを取得するvector
-    vector<cv::Mat> vec_image;      //画像データ保存用のvector
-    int frame_count = 0; //現在のFrame位置を記録しておく変数
-    uint32_t temp_time = NULL; //FPS算出用の変数
-    std::string timedata = getDatetimeStr(); //現在の時間を取得
-    std::string dir = CreateSaveDir(timedata); //時間が名前のフォルダを作成する
-
-    do
-    {
-        k4a_capture_t sensor_capture;
-        k4a_wait_result_t get_capture_result;
-        get_capture_result = k4a_device_get_capture(device, &sensor_capture, K4A_WAIT_INFINITE);
-        if (get_capture_result == K4A_WAIT_RESULT_SUCCEEDED)
-        {
-            frame_count++;
-            k4a_wait_result_t queue_capture_result =\
-                k4abt_tracker_enqueue_capture(tracker, sensor_capture, K4A_WAIT_INFINITE);
-            //k4a_capture_release(sensor_capture); // Remember to release the sensor capture once you finish using it
-            printf("%u result", queue_capture_result);
-            if (cap_result(queue_capture_result)==0) {
-                break;
-            }
-
-            k4a_wait_result_t pop_frame_result = \
-                k4abt_tracker_pop_result(tracker, &body_frame, K4A_WAIT_INFINITE);
-
-            //フレームの取得が成功したとき
-            if (pop_frame_result == K4A_WAIT_RESULT_SUCCEEDED)
-            {
-                // Successfully popped the body tracking result. Start your processing
-                uint32_t num_bodies = k4abt_frame_get_num_bodies(body_frame);
-                printf("%u body, ", num_bodies);
-
-                //現在の時刻を取得する（マイクロ秒の情報を取得)
-                uint32_t now_time = \
-                    duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-                if (frame_count > 1) {
-                    int FPS = (int)1000 / (now_time - temp_time);
-                    printf("%dFPS \n", FPS);
-                }
-                temp_time = now_time;
-                uint32_t during_millisec = (now_time - start_time);
-        
-                //std::string str_during_millisec = std::to_string(during_millisec);
-                //std::cout << now;
-                for (uint32_t i = 0; i < num_bodies; i++)
-                {
-                    k4a_result_t result = k4abt_frame_get_body_skeleton(body_frame, i, &skeleton);
-                    if (result == K4A_RESULT_FAILED)
-                    {
-                        printf("Error! cannot capture result\n");
-                        break;
-                    }
-                    uint32_t id = k4abt_frame_get_body_id(body_frame, i); //idを格納
-
-                    list<string> l_joint_2d = Camera3dto2d(during_millisec, id);
-                    //vectorにいれる
-                    vec_jointlist_2D.push_back(l_joint_2d);
-
-                    //3D camera dataを格納するリストの作成
-                    list<string> l_joint;
-                    l_joint.push_back(std::to_string(during_millisec));
-                    l_joint.push_back(std::to_string(id));
-                    //printf("id %zu is detected!!!\n", id);
-        
-                    //フレームのi番のスケルトンデータ
-                    for (int jointId = 0; jointId < 33; ++jointId)
-                    {   
-                        k4abt_joint_t joint = skeleton.joints[jointId];
-                        //printf("jointId = %d position is %f.\n", jointId, joint.position.xyz.x);
-                        //実際の書き込み
-                        std::string x = std::to_string(joint.position.xyz.x);
-                        std::string y = std::to_string(joint.position.xyz.y);
-                        std::string z = std::to_string(joint.position.xyz.z);
-                        l_joint.push_back(x);
-                        l_joint.push_back(y);
-                        l_joint.push_back(z);
-                    }
-                    //vectorにいれる
-                    vec_jointlist.push_back(l_joint);
-                }
-                k4abt_frame_release(body_frame); // Remember to release the body frame once you finish using it
-
-                // Kinect for Azure color & depth.
-                const auto k4a_color = k4a_capture_get_color_image(sensor_capture);
-
-                // Get color as cv::
-                const auto cv_color = cv::Mat_<cv::Vec4b>(
-                    k4a_image_get_height_pixels(k4a_color),
-                    k4a_image_get_width_pixels(k4a_color),
-                    (cv::Vec4b*)k4a_image_get_buffer(k4a_color),
-                    k4a_image_get_stride_bytes(k4a_color));
-                cv::imshow("color", cv_color);
-                //vectorにcolor画像を格納
-                vec_image.push_back(cv_color);
-                char dir[30] = "KinectImage";
-                //cv::imwrite(&dir + "/" + std::to_string(during_millisec) + ".jpg", cv_color);
-            
-                cv::waitKey(1);
-                k4a_image_release(k4a_color);
-                k4a_capture_release(sensor_capture);
-            }
-            else if (pop_frame_result == K4A_WAIT_RESULT_TIMEOUT)
-            {
-                //  It should never hit timeout when K4A_WAIT_INFINITE is set.
-                printf("Error! Pop body frame result timeout!\n");
-                break;
-            }
-            else
-            {
-                printf("Pop body frame result failed!\n");
-                break;
-            }
-        }
-        //エラー処理
-        else if (get_capture_result == K4A_WAIT_RESULT_TIMEOUT)
-        {
-            // It should never hit time out when K4A_WAIT_INFINITE is set.
-            printf("Error! Get depth frame time out!\n");
-            break;
-        }
-        else
-        {
-            printf("Get depth capture returned error: %d\n", get_capture_result);
-            break;
-        }
-
-        //キー入力待機
-        if (_kbhit()) {
-            if (getchar() == 'q') {
-                printf("stop recording\n");
-                break;
-            }
-        }
-    } while (frame_count < FRAME_NUM);
-
-    //file.close();
-    k4abt_tracker_shutdown(tracker);
-    k4abt_tracker_destroy(tracker);
-    k4a_device_stop_cameras(device);
-    k4a_device_close(device);
-    
-    printf("saving");
-    
+void Save3dPoint(string timedata) {
     //3Dの関節点のデータを書き込む用のファイルを作成
     std::ofstream writing_file;
     std::string filename = "./jointdata_csv/" + timedata + ".csv";
@@ -293,38 +162,190 @@ int main()
             writing_file << ',';
         }
         writing_file << '\n';
-        printf(".");
+        cout << ".";
     }
     printf("\n");
     writing_file.close();
+}
 
-    //2Dの関節点のデータを書き込む用のファイルを作成
-    std::ofstream writing_file2;
-    std::string filename2 = "./jointdata_csv_2D/" + timedata + ".csv";
-    writing_file2.open(filename2, std::ios::out);  //ファイルを開く
+//キャリブレーションを行った画面描写用の2D関節点情報をファイルに保存する
+void Save2dColor(string timedata) {
+    std::ofstream writing_file;
+    std::string filename = "./jointdata_csv_2D/" + timedata + ".csv";
+    writing_file.open(filename, std::ios::out);  //ファイルを開く
     //ファイルへのデータの書き込み
-    for (const auto& e : vec_jointlist_2D) {
+    for (const auto& e : vec_jointlist_2d) {
         list<string> l = e;
         for (string s : l) {
             writing_file << s;
             writing_file << ',';
         }
         writing_file << '\n';
-        printf(".");
+        cout << ".";
     }
     printf("\n");
     writing_file.close();
 
+}
 
-    for (int i=0; i < vec_image.size() ; i++) {
-        
-        //cv::imshow("color", vec_image[i]);
-        //cv::imwrite(dir_str + "/" + *itr + ".jpg", vec_image[i]);
-        //printf("%d ", i);
+// color Imageを保存する
+void SaveImg(vector<cv::Mat_<cv::Vec4b>> vec_image, vector<string>vec_time, string timedata) {
+    std::size_t size = vec_image.size();
+    for (unsigned int i = 0; i < size; i++) {
+        cv::imwrite("KinectImage/" + timedata + "/" + vec_time[i] + ".jpg", vec_image[i]);
+        cout << ".";
     }
-    
-    cv::destroyAllWindows();
-    printf("\nFinished body tracking processing!\n");
+}
 
+void Proc(uint32_t start_time) {
+    k4a_capture_t sensor_capture;
+    k4a_wait_result_t get_capture_result;
+    get_capture_result = k4a_device_get_capture(device, &sensor_capture, K4A_WAIT_INFINITE);
+    if (get_capture_result == K4A_WAIT_RESULT_SUCCEEDED)
+    {
+        frame_count++;
+        k4a_wait_result_t queue_capture_result = \
+            k4abt_tracker_enqueue_capture(tracker, sensor_capture, K4A_WAIT_INFINITE);
+        //k4a_capture_release(sensor_capture); // Remember to release the sensor capture once you finish using it
+        //printf("%u result", queue_capture_result);
+        if (cap_result(queue_capture_result) == 0) {
+            MessageBox(NULL, TEXT("結果をキャプチャーできませんでした"), TEXT("エラー"), MB_OK);
+        }
+
+        k4a_wait_result_t pop_frame_result = \
+            k4abt_tracker_pop_result(tracker, &body_frame, K4A_WAIT_INFINITE);
+
+        //フレームの取得が成功したとき
+        if (pop_frame_result == K4A_WAIT_RESULT_SUCCEEDED)
+        {
+            // Successfully popped the body tracking result. Start your processing
+            uint32_t num_bodies = k4abt_frame_get_num_bodies(body_frame);
+            std::cout << "  number of body = " << num_bodies << "\n";
+
+            //現在の時刻を取得する（マイクロ秒の情報を取得)
+            uint32_t now_time = \
+                duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+            if (frame_count > 1) {
+                int FPS = (int)1000 / (now_time - temp_time);
+                std::cout << FPS << "FPS";
+            }
+            temp_time = now_time;
+            uint32_t during_millisec = (now_time - start_time);
+
+            //std::string str_during_millisec = std::to_string(during_millisec);
+            //std::cout << now;
+            for (uint32_t i = 0; i < num_bodies; i++)
+            {
+                k4a_result_t result = k4abt_frame_get_body_skeleton(body_frame, i, &skeleton);
+                if (result == K4A_RESULT_FAILED)
+                {
+                    MessageBox(NULL, TEXT("骨格情報を取得できませんでした。"), TEXT("エラー"), MB_OK);
+                }
+                uint32_t id = k4abt_frame_get_body_id(body_frame, i); //idを格納
+
+                list<string> l_joint_2d = Camera3dto2d(during_millisec, id);
+                //vectorにいれる
+                vec_jointlist_2d.push_back(l_joint_2d);
+
+                //3D camera dataを格納するリストの作成
+                list<string> l_joint;
+                l_joint.push_back(std::to_string(during_millisec));
+                l_joint.push_back(std::to_string(id));
+                //printf("id %zu is detected!!!\n", id);
+
+                //フレームのi番のスケルトンデータ
+                for (int jointId = 0; jointId < 33; ++jointId)
+                {
+                    k4abt_joint_t joint = skeleton.joints[jointId];
+                    //printf("jointId = %d position is %f.\n", jointId, joint.position.xyz.x);
+                    //実際の書き込み
+                    std::string x = std::to_string(joint.position.xyz.x);
+                    std::string y = std::to_string(joint.position.xyz.y);
+                    std::string z = std::to_string(joint.position.xyz.z);
+                    l_joint.push_back(x);
+                    l_joint.push_back(y);
+                    l_joint.push_back(z);
+                }
+                //vectorにいれる
+                vec_jointlist.push_back(l_joint);
+            }
+            k4abt_frame_release(body_frame); // Remember to release the body frame once you finish using it
+
+            // Kinect for Azure color & depth.
+            const auto k4a_color = k4a_capture_get_color_image(sensor_capture);
+
+            // Get color as cv::
+            const auto cv_color = cv::Mat_<cv::Vec4b>(
+                k4a_image_get_height_pixels(k4a_color),
+                k4a_image_get_width_pixels(k4a_color),
+                (cv::Vec4b*)k4a_image_get_buffer(k4a_color),
+                k4a_image_get_stride_bytes(k4a_color));
+            cv::imshow("color", cv_color);
+            //vectorにcolor画像を格納
+            vec_image.push_back(cv_color);
+            vec_time.push_back(to_string(during_millisec));
+
+            cv::waitKey(1);
+            //k4a_image_release(k4a_color); //これあるとポインタが被って正しく保存できない。
+            k4a_capture_release(sensor_capture);
+        }
+        else if (pop_frame_result == K4A_WAIT_RESULT_TIMEOUT)
+        {
+            //  It should never hit timeout when K4A_WAIT_INFINITE is set.
+            //printf("Error! Pop body frame result timeout!\n");
+            MessageBox(NULL, TEXT("Error! Pop body frame result timeout!\n"), TEXT("エラー"), MB_OK);
+
+        }
+        else
+        {
+            printf("Pop body frame result failed!\n");
+            MessageBox(NULL, TEXT("Pop body frame result failed!\n"), TEXT("エラー"), MB_OK);
+        }
+    }
+    //エラー処理
+    else if (get_capture_result == K4A_WAIT_RESULT_TIMEOUT)
+    {
+        // It should never hit time out when K4A_WAIT_INFINITE is set.
+        printf("Error! Get depth frame time out!\n");
+        MessageBox(NULL, TEXT("Error! Get depth frame time out!\n"), TEXT("エラー"), MB_OK);
+    }
+    else
+    {
+        printf("Get depth capture returned error: %d\n", get_capture_result);
+        MessageBox(NULL, TEXT("Get depth capture returned error:\n"), TEXT("エラー"), MB_OK);
+    }
+}
+
+//実プログラム
+int main()
+{
+    CreateKinect(); //Kinectの立ち上げ
+    //計測開始時のtimesramp作成
+    uint32_t start_time = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    std::string timedata = getDatetimeStr(); //現在の時間を取得
+    std::string dir = CreateSaveDir(timedata); //timestampを保持した名前のフォルダを作成する
+
+    while (frame_count < FRAME_NUM) {
+        Proc(start_time); // 繰り返し動作、スケルトン情報の取得など
+        //キー入力待機 qを押下したら動作停止
+        if (_kbhit()) {
+            if (getchar() == 'q') {
+                printf("キャプチャが終了しました\n");
+                break;
+            }
+        }
+    }
+    cout << "saving csv data\n";
+    //3Dの関節点データを書き込む
+    Save3dPoint(timedata);
+    //画面描写用の2Dの関節点のデータを書き込む
+    Save2dColor(timedata);
+
+    //画像データの保存
+    cout << "saving img data\n";
+    SaveImg(vec_image, vec_time, timedata);
+
+    DestroyKinect();
     return 0;
 }
+
